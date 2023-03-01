7aeb9e2f70c393fdbf15737e48e60d001039882f
riccardo andronache <dev.riccardo.a@gmail.com>
2023-03-01 09:36:28 +0200

    feat: debugger

--- infra/SpaceStack.ts
+++ infra/SpaceStack.ts
@@ -6,6 +6,7 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { join } from 'path';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, 'SpaceApi');
@@ -18,6 +19,12 @@ export class SpaceStack extends Stack {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler',
     });
+
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*');
+
+    helloLambdaNodejs.addToRolePolicy(s3ListPolicy);
 
     // Hello Api lambda integration
     const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodejs);
